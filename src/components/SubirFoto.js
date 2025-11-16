ed4d1f67bfbaa1963926d94008fc99e263154bbf
AlbaErdociainHerrero97 <135984435+AlbaErdociainHerrero97@users.noreply.github.com>
2025-11-16 20:55:51 +0100

    cambio de variables que no se pueden visualizar

--- src/components/SubirFoto.js
+++ src/components/SubirFoto.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { db } from "../firebase";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
-import IMGBB_API_KEY from "../config.js";
+import {IMGBB_API_KEY_original} from "../config.js";
 
-const IMGBB_API_KEY = IMGBB_API_KEY; // Pon aquí tu API key
+const IMGBB_API_KEY = IMGBB_API_KEY_original; // Pon aquí tu API key
 
 const SubirFoto = () => {
   const [nombre, setNombre] = useState("");
